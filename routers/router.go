6fbf54798f0f7670384ced9dfa3e4b40cd3a6aeb
itswcg <itswcg@gmail.com>
2019-06-16 17:41:04 +0800

    feat: add article

--- routers/router.go
+++ routers/router.go
@@ -15,10 +15,25 @@ func InitRouter() *gin.Engine {
 
 	api_v1 := r.Group("/api/v1")
 	{
+		//获取标签列表
 		api_v1.GET("/tags", v1.GetTags)
+		//新建标签
 		api_v1.POST("/tags", v1.AddTag)
+		//更新指定标签
 		api_v1.PUT("/tags/:id", v1.EditTag)
+		//删除指定标签
 		api_v1.DELETE("/tags/:id", v1.DeleteTag)
+
+		//获取文章列表
+		api_v1.GET("/articles", v1.GetArticles)
+		//获取指定文章
+		api_v1.GET("/articles/:id", v1.GetArticle)
+		//新建文章
+		api_v1.POST("/articles", v1.AddArticle)
+		//更新指定文章
+		api_v1.PUT("/articles/:id", v1.EditArticle)
+		//删除指定文章
+		api_v1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 
 	return r
